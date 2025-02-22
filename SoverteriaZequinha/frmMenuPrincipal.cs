1df54351082038a3a9eb7dde13639acfcc052326
Andre855-bit <andremachado855@gmail.com>
2025-02-21 21:49:12 -0300

    commit 01

--- SoverteriaZequinha/frmMenuPrincipal.cs
+++ SoverteriaZequinha/frmMenuPrincipal.cs
@@ -16,5 +16,20 @@ namespace SoverteriaZequinha
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
